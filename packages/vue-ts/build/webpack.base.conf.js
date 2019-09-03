938cda1c52421e167cf5bb212822a3e420743ffa
wx-chevalier <384924552@qq.com>
2019-09-03 21:19:33 +0800

    feat: update articles

---
+++ packages/vue-ts/build/webpack.base.conf.js
@@ -0,0 +1,99 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
+var vueloader = require('vue-loader')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+ // context: resolve( 'src'),
+  entry: { app: resolve('./src/app.ts')
+  //,'tether': 'tether'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js','.ts','.vue','.json'],
+    alias: {
+      'src': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets'),
+      'components': path.resolve(__dirname, '../src/components'),
+      'vue$': 'vue/dist/vue.esm.js',      
+      '@': resolve('src'),
+      'System': path.resolve(__dirname, '../src/System'),
+      'Views': path.resolve(__dirname, '../src/Views'),
+      'ext1': path.resolve(__dirname, '../src/ext1')
+    }
+  },
+  module: {
+    rules: [    
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          esModule: true
+        }
+      
+      },
+ 
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+    
+       // include: [resolve('src')],
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+  
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+      
+    ]
+  },
+  plugins: [
+    new vueloader.VueLoaderPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.Jquery': "jquery",          
+      'window.Tether': "tether",
+      "Tether": "tether"
+     // "window.Sortable": 'sortablejs'
+})
+
+]
+
+}
